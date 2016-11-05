396fcdd5eb08084eb9ed707d76b8d8acc4f01d00
rmore071 <rmore071@fiu.edu>
2016-11-05 13:02:38 -0400

    MSED
    Multipart Equipment Dashboard

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var connection = mysql.createConnection({
+  host     : 'wp-msed-db.c9wr0yzscnts.us-east-1.rds.amazonaws.com',
+  user     : 'msed_db',
+  password : 'cen4083cen4083',
+  database : 'mseddb',
+  port     : '3306'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM activities', function(err, rows, fields) {
+  if (err) throw err;
+  console.log(rows[0].activityId);
+});
+
+connection.end();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
